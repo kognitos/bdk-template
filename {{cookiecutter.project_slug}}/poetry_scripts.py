696aea3324b4009d7cdb05f7a68885cbd0efbcc4
Matias Ratcliffe <matias@kognitos.com>
2025-01-21 12:26:37 -0300

    feat: added script to test locally

--- {{cookiecutter.project_slug}}/poetry_scripts.py
+++ {{cookiecutter.project_slug}}/poetry_scripts.py
@@ -25,3 +25,14 @@ def run_type_check():
 def run_doc():
     # generate documentation
     return run_cmd("poetry bdk usage")
+
+def run_host():
+    # build and run docker image
+    runtime_version = toml.load('pyproject.toml')['environment'].get('bdk_runtime_version', 'latest')
+    bdk_runtime_uri = f"kognitosinc/bdk:{runtime_version}"
+    image_tag = "{{ cookiecutter.project_slug }}:local_test"
+    run_cmd(f"docker build --build-arg BDK_RUNTIME_IMAGE_URI={bdk_runtime_uri} -t {image_tag} .")
+    ngrok_token = os.getenv("NGROK_AUTHTOKEN")
+    if not ngrok_token:
+        raise ValueError("Missing NGROK api key")
+    run_cmd(f"docker run --rm -e BDK_SERVER_MODE=ngrok -e NGROK_AUTHTOKEN={ngrok_token} {image_tag}")
